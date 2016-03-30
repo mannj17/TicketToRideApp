839256edcda189a9593b7db7ea84ae89d6ff1d01
mannj17 <mannj17@up.edu>
2016-03-30 15:27:25 -0700

    TrainCards updated

--- app/src/main/java/teamresistance/tickettoride/TTR/TrainCards.java
+++ app/src/main/java/teamresistance/tickettoride/TTR/TrainCards.java
@@ -1,7 +1,7 @@
 package teamresistance.tickettoride.TTR;
 
 /**
- *
+ * Train Card class. Contains the train card colors and sets the card to a specific type.
  *
  * @author Nick Scacciotti
  * @author Nick Larson
@@ -10,27 +10,70 @@ package teamresistance.tickettoride.TTR;
  * @version March 2016
  */
 public class TrainCards {
+    //String of names of the different train card colors
+    private final String[] trainCarNames = {"Yellow", "Blue", "Orange", "White",
+            "Pink", "Black", "Red", "Green",
+            "Rainbow"};
 
-
-    /*
-     * Array of all the colors used for train cards
-     */
-
-    private String[] trainCarNames;
-
-    /*
-     * the type of train card
-     */
-
+    //instance variables for what color type and if highlighted
     private String type;
+    private boolean highlight;
 
-
-    /*
-     * Sets the variables for a given DestCards
-     * @typeNum
+    /**
+     * Constructor for TrainCards class
+     *
+     * @param typeNum the int value for the color type in the
+     *                trainCarNames array     *
      */
+    public TrainCards(int typeNum){
+        type = trainCarNames[typeNum];
+    }
 
-    public TrainCards(int typeNum) {
-    
+    /**
+     * Gives the color type of the Train Card
+     *
+     * @return
+     *      Returns the type of card
+     */
+    public String toString(){
+        return this.type;
+    }
+
+    /**
+     * Sets the highlight variable to the new variable
+     *
+     * @param highlighted- new boolean value for if highlighted or not
+     */
+    public void setHighlight(boolean highlighted){
+        this.highlight = highlighted;
+    }
+
+    /**
+     * Tells whether the Destination ticket is highlighted or not
+     *
+     * @return
+     *      Returns true or false
+     */
+    public boolean getHighlight(){
+        return highlight;
+    }
+
+    /**
+     * Gives the color type of the Train Card
+     *
+     * @return
+     *      Returns the card type
+     */
+    public String getType() {
+        return type;
+    }
+
+    /**
+     * Sets the Train card type to a new color type
+     *
+     * @param type - new color type
+     */
+    public void setType(String type) {
+        this.type = type;
     }
 }
