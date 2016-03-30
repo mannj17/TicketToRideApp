9fe68370805bf2bf573156565befc15cbd52492b
mannj17 <mannj17@up.edu>
2016-03-30 15:37:24 -0700

    Card classes updated

--- app/src/main/java/teamresistance/tickettoride/TTR/Card.java
+++ app/src/main/java/teamresistance/tickettoride/TTR/Card.java
@@ -10,4 +10,23 @@ package teamresistance.tickettoride.TTR;
  * @version March 2016
  */
 public class Card {
+    private boolean highlight;
+    /**
+     * Sets the highlight variable to the new variable
+     *
+     * @param highlighted- new boolean value for if highlighted or not
+     */
+    public void setHighlight(boolean highlighted){
+        this.highlight = highlighted;
+    }
+
+    /**
+     * Tells whether the Destination ticket is highlighted or not
+     *
+     * @return
+     *      Returns true or false
+     */
+    public boolean getHighlight(){
+        return highlight;
+    }
 }
