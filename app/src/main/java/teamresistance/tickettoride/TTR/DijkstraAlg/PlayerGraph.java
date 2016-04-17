39aaefc086fc698d28f80c5e0500101e9b64d06b
Jess <mannj17@up.edu>
2016-04-17 13:33:38 -0700

    Player graph

--- app/src/main/java/teamresistance/tickettoride/TTR/DijkstraAlg/PlayerGraph.java
+++ app/src/main/java/teamresistance/tickettoride/TTR/DijkstraAlg/PlayerGraph.java
@@ -1,7 +1,29 @@
 package teamresistance.tickettoride.TTR.DijkstraAlg;
 
+import java.util.List;
+
 /**
  * Created by Jess on 4/17/2016.
  */
 public class PlayerGraph {
+
+    private final int playerNum;
+    private List<Edge> edges;
+    private List<Vertex> vertexes;
+
+    public PlayerGraph(List<Vertex> vertexes, List<Edge> edges, int playerNum) {
+        this.vertexes = vertexes;
+        this.edges = edges;
+        this.playerNum = playerNum;
+    }
+
+    public List<Vertex> getVertexes() {
+        return vertexes;
+    }
+
+    public List<Edge> getEdges() {
+        return edges;
+    }
+
 }
+
